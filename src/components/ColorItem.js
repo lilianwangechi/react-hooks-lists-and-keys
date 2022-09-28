d88649b8ad94ebd1c32cc47809a9b8f66f818ffd
lilian <lilian.wangechi@student.moringaschool.com>
2022-09-28 13:56:51 +0300

    done

---
+++ src/components/ColorItem.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function ColorItem({color}){
+  return <li style={{ color:color}}>{color}</li>
+}
+export default ColorItem;
--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ColorItem from "./ColorItem";
 
 function ColorList() {
   const colors = [
@@ -9,18 +10,36 @@ function ColorList() {
     "hotpink",
   ];
 
-  return (
-    <div>
-      <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
-    </div>
-  );
+const colorElements = colors.map((color)=>{
+ // return <li style={{color:color}}>{color}</li>;
+ //when we need to create a seperate component to display each color put the key prop in the ColorItem and not <li></li>
+ return <ColorItem key={color} color={color}/>
+
+});
+
+return (
+  <div>
+    <h1>Top 5 CSS colors</h1>
+    <ol>
+      {/* {display the array of<li> elements here!} */}
+    {colorElements}
+     </ol>
+  </div>
+);
 }
+
+//   return (
+//     <div>
+//       <h1>Top 5 CSS Colors</h1>
+//       <ol>
+//         <li style={{ color: colors[0] }}>{colors[0]}</li>
+//         <li style={{ color: colors[1] }}>{colors[1]}</li>
+//         <li style={{ color: colors[2] }}>{colors[2]}</li>
+//         <li style={{ color: colors[3] }}>{colors[3]}</li>
+//         <li style={{ color: colors[4] }}>{colors[4]}</li>
+//       </ol>
+//     </div>
+//   );
+// }
 
 export default ColorList;
